01ac29079244196b73257981bcf6e49da0d7d0fa
Deepak Raikwar <raikwardeepak310@gmail.com>
2026-07-17 22:49:56 +0530

    Disable PWA for testing

--- vite.config.js
+++ vite.config.js
@@ -1,43 +1,43 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    VitePWA({
-      registerType: "autoUpdate",
-      injectRegister: "auto",
-      base: "/",
-      manifest: {
-        id: "/",
-        name: "NewsHub",
-        short_name: "NewsHub",
-        description: "Latest news from around the world",
+//     VitePWA({
+//       registerType: "autoUpdate",
+//       injectRegister: "auto",
+//       base: "/",
+//       manifest: {
+//         id: "/",
+//         name: "NewsHub",
+//         short_name: "NewsHub",
+//         description: "Latest news from around the world",
 
-        start_url: "/",
-        scope: "/",
+//         start_url: "/",
+//         scope: "/",
 
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#2563eb",
+//         display: "standalone",
+//         background_color: "#ffffff",
+//         theme_color: "#2563eb",
 
-        icons: [
-          {
-            src: "icons/icon-192.png",
-            sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "icons/icon-512.png",
-            sizes: "512x512",
-            type: "image/png"
-          }
-        ]
-    }
-})
+//         icons: [
+//           {
+//             src: "icons/icon-192.png",
+//             sizes: "192x192",
+//             type: "image/png"
+//           },
+//           {
+//             src: "icons/icon-512.png",
+//             sizes: "512x512",
+//             type: "image/png"
+//           }
+//         ]
+//     }
+// })
   ],
   base: "/",
 });
